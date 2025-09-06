d302dfebb42460d5b391f3d58bc8fa81052c9d5e
Malaiyarasi <malaiyarasi249@gmail.com>
2025-09-06 19:10:41 +0530

    updated

---
+++ JavaSript/js_files/keywords.js
@@ -0,0 +1,53 @@
+
+let personname='lohith';
+{
+    console.log(personname)
+    console.log(typeof(personname))
+}
+
+let no=123.5;
+{
+    console.log(no)
+    console.log(typeof(no))
+}
+
+let status=false;
+{
+    console.log(status)
+    console.log(typeof(status))
+}
+
+let name1=null;
+{
+    console.log(name1)
+}
+
+let mydeclare;
+{
+    console.log(mydeclare)
+}
+
+const course=['python',85.1,true,'dotnet',155,12.5,'sequel',false,'html','javascript']
+{
+    console.log(course)
+    console.log(typeof(course)) 
+}
+
+const user={
+    names:'jayashree',
+    age:31,
+    gender:'female',
+    email:'jaya@yahoo.com',
+    phone:935412454
+};
+
+
+
+
+
+
+
+
+
+
+
